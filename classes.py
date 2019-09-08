7c8378e359eb14087e4ed5acebddc20cf5d3521b
root <root@LAPTOP-Q4UJMUND.localdomain>
2019-09-08 15:25:27 -0400

    added card 7.2

--- classes.py
+++ classes.py
@@ -1,5 +1,18 @@
 class Trail():
     """A class to represent trails."""
-    def __init__(seld, dest, len=0):
+    def __init__(self, dest, len=0):
         self.dest = dest
-        self.len = len()
+        self.len = len
+
+    def describe_trail(self):
+        """Print a description of trail."""
+        dest = f'this trail goes to {self.dest}.'
+
+        if self.len:
+            dest += f'\nThe trail is {self.len}km.'
+        print(dest)
+
+
+mytrail = Trail('Tawoco', 20)
+
+mytrail.describe_trail()
